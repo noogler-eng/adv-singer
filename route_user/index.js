2ec7164c9881cc66bd497515d1f81816b6665a9d
shared <poddarsharad460@gmail.com>
2024-08-08 11:33:10 +0530

    added

--- route_user/index.js
+++ route_user/index.js
@@ -26,24 +26,25 @@ user_route.get("/me", async(req, res)=>{
 })
 
 // getting user all top tracks where limit is 10
-user_route.get("me/tracks", async(req, res)=>{
-    const access_token = req.body.access_token;
-    
-    try{
-        const response = await axios.get(
-          "https://api.spotify.com/v1/me/top/tracks", {
-            headers: {
-              Authorization: "Bearer " + access_token,
-            },
+user_route.get("/me/tracks", async(req, res)=>{
+  const access_token = await req.headers.authorization;
+    console.log(access_token);
+
+  try{
+      const response = await axios.get(
+        "https://api.spotify.com/v1/me/top/tracks", {
+          headers: {
+            Authorization: "Bearer " + access_token,
           },
-        );
-        const data = await response.data;
-    
-        res.json({ msg: data });
-    }catch (error) {
-        console.log(error);
-        res.status(500).json({ msg: "data retrival unssessfull" });
-    }
+        },
+      );
+      const data = await response.data;
+  
+      res.json({ msg: data });
+  }catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: "data retrival unssessfull" });
+  }
 })
 
 // getting user all top artists where limit is 10
